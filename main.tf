db5b04e5c134ce4639a02fafdab9391a6a891682
Manfred Briese <mbriesejr@gmail.com>
2024-01-12 13:10:28 -0800

    Update bucket name

--- main.tf
+++ main.tf
@@ -123,7 +123,7 @@ resource "aws_route_table_association" "private_subnet_asso" {
 
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "mybucket3445345656457676878687867867867"
+  bucket = "mybucketmanfred3445345656457676878687867867867"
   acl    = "private"
   force_destroy = true
   lifecycle {
